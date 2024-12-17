26f023ce92066dafd766c654fd9176921a1375e9
xmas <xmas@iesdamiahuguet.net>
2024-12-17 13:03:40 +0100

    Afegida funció suma

--- funcions.js
+++ funcions.js
@@ -60,3 +60,9 @@ function valorsentre() {
     document.getElementById('valors').innerHTML = text_valors;
     document.getElementById('valors').hidden =false;
 }
+
+function suma(n1,n2) {
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    alert(n1+n2);
+}
